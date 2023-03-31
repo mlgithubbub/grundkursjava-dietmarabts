e5739fc04771bc71c30231a471078a2a46848d99
mlgithubbub <to.morganlinn@gmail.com>
2023-03-31 15:15:27 +0200

    2023-03-31 First commit

---
+++ src/main/java/de/javagrundlagen/Kapitel01/Demo1.java
@@ -0,0 +1,8 @@
+package de.javagrundlagen.Kapitel01;
+
+public class Demo1 {
+
+    public static void main(String[] args){
+        System.out.println("Viel Erfolg mit dem Grundkurs Java");
+    }
+}
