f06bab26c989370bbdf7e485365546f9fadb3737
mlgithubbub <to.morganlinn@gmail.com>
2023-06-12 14:11:50 +0200

    kapitel 3 aufgabe 4

---
+++ src/main/java/de/javagrundlagen/Kapitel03Aufgaben/Aufgabe04.java
@@ -0,0 +1,31 @@
+package de.javagrundlagen.Kapitel03Aufgaben;
+
+public class Aufgabe04 {
+
+    /*
+    FRAGE
+    Welche Werte haben die folgenden Ausdrücke und welche Werte haben die Variablen nach der Auswertung,
+    wenn a den Anfangswert 1 und b den Anfangswert 7 hat?
+
+    a) --a
+    b) a--
+    c) a++ + b
+    d) b = ++a
+    e) a = b++
+    f) -(a--) - -(--b)
+    g) a++ + ++a + a++
+
+    ANTWORT
+    Wert --> Wert nach der Auswertung
+    a) 0 --> 0
+    b) 1 --> 0
+    c) 0 + 7 = 7 --> 1 + 7 = 8
+    d) b = 2
+    e) a = b++
+    f) -(a--) - -(--b)
+    g) a++ + ++a + a++
+
+    */
+
+
+}
